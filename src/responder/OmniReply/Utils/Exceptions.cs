c9d356c335f11b7f2804ae19917f522bdcd261a3
ParaN3xus <136563585+ParaN3xus@users.noreply.github.com>
2024-02-29 23:21:53 +0800

    refactor: remove useless utils

---
+++ src/responder/OmniReply/Utils/Exceptions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OmniReply.Utils
+{
+    public class Exceptions
+    {
+        public class SandBoxTimeoutException : Exception
+        {
+            public SandBoxTimeoutException() : base("Timeout!")
+            {
+            }
+        }
+    }
+}
